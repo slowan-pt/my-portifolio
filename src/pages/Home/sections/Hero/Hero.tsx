2b031c81c955058cbfbf26a4b4709b5df322440e
Sloan Nascimento <sloan.nascimento@gmail.com>
2025-01-08 05:38:48 -0300

    ALT: hero.tsx -> Inserido avatar e iniciada estilizacao

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,24 +1,35 @@
-import { styled } from '@mui/material'
+import { Container, Grid, Grid2, styled } from '@mui/material'
 import Avatar from "../../../../assets/images/avatar.png"
 
 
 
 const Hero = () => {
 
-    const StyleHero = styled("div")(()=> ({
-        backgroundColor: 'black'
-    }))
+     const StyledHero = styled("div")(()=> ({
+         backgroundColor: 'black'
+     }))
 
-    const StyleImg = styled("img")(()=> ({
-        
+    const StyledImg = styled("img")(()=> ({
+        width: "30%",
+        borderRadius: "50%"
     }))
 
   return (
     <>
-     <StyleHero>
-        Ola
-        <img src={Avatar} />
-     </StyleHero>
+     <StyledHero>
+      <Container>
+
+     
+          <Grid2 container spacing={2}>
+              <Grid2 item xs={4}>
+                <StyledImg src={Avatar} />
+              </Grid2>
+              <Grid2 item xs={8}>
+                xs=4
+              </Grid2>
+          </Grid2>
+      </Container>
+     </StyledHero>
     </>
   )
 }
