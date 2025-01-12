44b526c68fb05c7533605fca7db763ede437c221
Sloan Nascimento <sloan.nascimento@gmail.com>
2025-01-12 08:50:09 -0300

    Go: Layout responsive ALT

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,16 +1,21 @@
-import { Container, Grid, Grid2, styled } from '@mui/material'
-import Avatar from "../../../../assets/images/avatar.png"
+import { Button, Container, styled, Typography } from '@mui/material'
+import Grid from '@mui/material/Grid2';
 
+
+import Avatar from "../../../../assets/images/avatar.png"
+import DownloadIcon from '@mui/icons-material/Download';
+import MailIcon from '@mui/icons-material/Mail';
 
 
 const Hero = () => {
 
      const StyledHero = styled("div")(()=> ({
-         backgroundColor: 'black'
+         backgroundColor: 'black',
+         height: '100vh'
      }))
 
     const StyledImg = styled("img")(()=> ({
-        width: "30%",
+        width: "100%",
         borderRadius: "50%"
     }))
 
@@ -18,17 +23,33 @@ const Hero = () => {
     <>
      <StyledHero>
       <Container>
-
-     
-          <Grid2 container spacing={2}>
-              <Grid2 item xs={4}>
+          <Grid container spacing={2}>
+              <Grid item xs={12} md={4}>
                 <StyledImg src={Avatar} />
-              </Grid2>
-              <Grid2 item xs={8}>
-                xs=4
-              </Grid2>
-          </Grid2>
+                  </Grid>
+                    <Grid item xs={12} md={8}>
+                      <Typography color="primary" variant="h1" textAlign="center">Sloan Nascimento</Typography>
+                      <Typography color="primary" variant="h2" textAlign="center">I'm a developer software</Typography>
+                
+                          <Grid container display="flex" justifyContent="center">
+                            <Grid item xs={12} md={6} display="flex" justifyContent="center">
+                                <Button>
+                                  <DownloadIcon/>
+                                  Download CV
+                                </Button>
+                            </Grid>
+                            <Grid item xs={12}  md={6} display="flex" justifyContent="center">
+                                <Button>
+                                  <MailIcon/>
+                                  Contacte me
+                                </Button>
+                            </Grid>
+                          </Grid>
+                    </Grid>
+                </Grid>
+                
       </Container>
+      
      </StyledHero>
     </>
   )
